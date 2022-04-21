ba41431701e03e42b224a390c2f216c297a19f76
kunihiro-dev <49485046+kunihiro-dev@users.noreply.github.com>
2022-04-21 21:27:41 +0900

    add sub menu

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,10 @@ use axum::Router;
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(handler));
+    let app = Router::new()
+        .route("/", get(handler))
+        .route("/sub", get(sub_handler));
+
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
         .serve(app.into_make_service())
         .await
@@ -13,4 +16,8 @@ async fn main() {
 
 async fn handler() -> Html<&'static str> {
     Html(include_str!("resources/index.html"))
+}
+
+async fn sub_handler() -> Html<&'static str> {
+    Html(include_str!("resources/sub.html"))
 }
