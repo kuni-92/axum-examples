10ec10bccf350707cf79a5475554ab1445a193a6
kunihiro-dev <49485046+kunihiro-dev@users.noreply.github.com>
2022-04-22 23:40:03 +0900

    print post form

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
+use axum::extract::Form;
 use axum::response::Html;
 use axum::routing::get;
 use axum::Router;
+use serde::Deserialize;
 
 #[tokio::main]
 async fn main() {
     let app = Router::new()
         .route("/", get(handler))
-        .route("/sub", get(sub_handler));
+        .route("/sub", get(sub_handler).post(sub_post_handler));
 
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
         .serve(app.into_make_service())
@@ -20,4 +22,13 @@ async fn handler() -> Html<&'static str> {
 
 async fn sub_handler() -> Html<&'static str> {
     Html(include_str!("resources/sub.html"))
+}
+
+#[derive(Deserialize, Debug)]
+struct Input {
+    name: String
+}
+
+async fn sub_post_handler(Form(input): Form<Input>) {
+    dbg!(&input);
 }
